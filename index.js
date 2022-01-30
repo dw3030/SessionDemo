dc5855601dded6270e7462929912c2fe2f1d4d36
Dustin Welch <dustinwwelch1@gmail.com>
2022-01-30 12:07:55 -0600

    session view count working

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "thisisnotagoodsecret",
+    // resave: false,
+    // saveUninitialized: false,
+  })
+);
+
+app.get("/viewcount", (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`You have viewed this page ${req.session.count} times`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
